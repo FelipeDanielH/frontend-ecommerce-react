382fbec375d7addabcfed552a2e3c55a11509160
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2025-05-01 21:23:01 -0400

    fix(hero): fixed button that doesnt redirect to catalog correctly

--- src/modules/Home/components/HeroSection.jsx
+++ src/modules/Home/components/HeroSection.jsx
@@ -1,15 +1,27 @@
 // src/modules/Home/components/HeroSection.jsx
+
+import { useNavigate } from "react-router-dom";
+
 export default function HeroSection() {
-    return (
-     <section className="bg-light py-4 border-bottom">
+
+  const navigate = useNavigate();
+
+  const handleNavigate = async () => {
+    navigate("/catalogo");
+  };
+
+  return (
+    <section className="bg-light py-4 border-bottom">
       <div className="container">
         <div className="row align-items-center">
           <div className="col-md-6 mb-4 mb-md-0">
             <h2 className="fw-bold">Encuentra todo lo que necesitas</h2>
             <p className="lead">Miles de productos de diferentes categorías al mejor precio.</p>
-            <a href="/catalogo" className="btn btn-green-custom fw-bold">
-              Explorar catálogo
-            </a>
+
+            <button onClick={handleNavigate} className="btn btn-green-custom fw-bold">
+            Explorar catálogo
+            </button>
+          
           </div>
           <div className="col-md-6 text-center">
             <img
@@ -21,6 +33,5 @@ export default function HeroSection() {
         </div>
       </div>
     </section>
-    );
-  }
-  
+  );
+}
